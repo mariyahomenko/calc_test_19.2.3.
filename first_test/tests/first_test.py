4ff0652cfb9a81e6867e7f1f67ecf2349631cb4a
mariyahomenko <73564203+mariyahomenko@users.noreply.github.com>
2021-03-10 02:26:59 +0300

    Update first_test.py

--- first_test/tests/first_test.py
+++ first_test/tests/first_test.py
@@ -6,21 +6,21 @@ class TestCalc:
         self.calc = Calculator
 
     def test_multiply_calculate_correctly(self):
-        assert self.calc.multiply(self, 5, 5) == 25
+        assert self.calc.multiply(self, 0.1, 10) == 1
     def test_multiply_calculation_failed(self):
-        assert self.calc.multiply(self, 5, 5) == 10
+        assert self.calc.multiply(self, 0.1, 10) == 1.0
 
     def test_division_calculate_correctly(self):
-        assert self.calc.division(self, 5, 5) == 1
+        assert self.calc.division(self, 73, 20) == 3.65
     def test_division_calculation_failed(self):
-        assert self.calc.division(self, 5, 5) == 0
+        assert self.calc.division(self, 73, 20) == 3
 
     def test_subtraction_calculate_correctly(self):
-        assert self.calc.subtraction(self, 5, 5) == 0
+        assert self.calc.subtraction(self, 2.6, 2) == 0.6
     def test_subtraction_calculation_failed(self):
-        assert self.calc.subtraction(self, 5, 5) == 1
+        assert self.calc.subtraction(self, 2.6, 2) == 0.6000000000000001
 
     def test_adding_calculate_correctly(self):
-        assert self.calc.adding(self, 5, 5) == 10
+        assert self.calc.adding(self, 0.2, 0.1) == 0.3
     def test_adding_calculation_failed(self):
-        assert self.calc.adding(self, 5, 5) == 25
+        assert self.calc.adding(self, 0.2, 0.1) == 0.30000000000000004
